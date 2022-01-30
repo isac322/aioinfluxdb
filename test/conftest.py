413609877d68f84a86c4744b640fb02a19412f59
Byeonghoon Yoo <bh322yoo@gmail.com>
2022-01-30 18:55:46 +0900

    ⚰ - Remove unused envs

--- test/conftest.py
+++ test/conftest.py
@@ -14,10 +14,6 @@ from aioinfluxdb import AioHTTPClient, types
 class InfluxDBConfig:
     host: str
     port: int
-    username: str
-    password: str
-    org: str
-    bucket: str
     admin_token: str
 
 
@@ -26,10 +22,6 @@ def influxdb_config() -> InfluxDBConfig:
     return InfluxDBConfig(
         host=os.environ['INFLUXDB_HOST'],
         port=os.environ.get('INFLUXDB_PORT', 8086),
-        username=os.environ['INFLUXDB_USERNAME'],
-        password=os.environ['INFLUXDB_PASSWORD'],
-        org=os.environ['INFLUXDB_ORG'],
-        bucket=os.environ['INFLUXDB_BUCKET'],
         admin_token=os.environ['INFLUXDB_ADMIN_TOKEN'],
     )
 
